e555d13728ef7aa70f923db8b983f5feab33964c
Daniel Mateu <danielmateu86@gmail.com>
2023-06-29 17:49:40 +0200

    My Countdown Solution

--- src/01-countdown/Countdown.tsx
+++ src/01-countdown/Countdown.tsx
@@ -6,8 +6,6 @@
 
   Validaciones son requeridas
 
-
-
 */
 
 import { useRef, useState } from "react"
@@ -17,24 +15,58 @@ import { RiPauseFill, RiPlayFill, RiRestartLine } from 'react-icons/ri';
 export const CountdownPage = () => {
 
   //TODO: hacer ejercicio entre 5 a 30 segundos
-  const [seconds, setSeconds] = useState(5);
+  const [seconds, setSeconds] = useState(10);
   const [isRunning, setIsRunning] = useState(false)
 
   const timerRef = useRef<number>();
 
   const startTimer = () => {
     // Todo: 
+
+    // 1. Si el timer esta corriendo, no hacer nada 
+    if (isRunning) return;
+
+    // 2. Si el timer no esta corriendo, iniciar el timer
+    setIsRunning(true);
+
+    timerRef.current = window.setInterval(() => {
+      setSeconds((prevSeconds) => {
+        if (prevSeconds === 0) {
+          window.clearInterval(timerRef.current);
+          setIsRunning(false);
+          return prevSeconds;
+        }
+        return prevSeconds - 1;
+      })
+    }, 1000);
+
   }
 
   const pauseTimer = () => {
     // Todo:
+
+    // 1. Si el timer no esta corriendo, no hacer nada
+    if (!isRunning) return;
+
+    // 2. Si el timer esta corriendo, detener el timer
+    setIsRunning(false);
+
+    // 3. Si el timer esta corriendo, detener el timer
+    window.clearInterval(timerRef.current);
+
+
   }
 
   const restartTimer = () => {
     // Todo:
+
+    // 1. Si el timer esta corriendo, detener el timer
+    window.clearInterval(timerRef.current);
+    // 2. Si el timer esta corriendo, detener el timer
+    setIsRunning(false);
+    // 3. Si el timer esta corriendo, detener el timer
+    setSeconds(10);
   }
-
-
 
   return (
     <div className="w-6/12 flex flex-col items-center">
--- src/02-radial/Radial.tsx
+++ src/02-radial/Radial.tsx
@@ -10,7 +10,7 @@
  * 
  */
 
-import { useState } from "react"
+import { useRef, useState } from "react"
 import { RiPauseFill, RiPlayLine, RiRestartLine } from "react-icons/ri";
 
 
@@ -18,50 +18,101 @@ interface Props {
   initialValue?: number;
 }
 
-export const RadialPage = ({ initialValue = 100 }: Props) => {
+export const RadialPage = ({ initialValue = 20 }: Props) => {
 
   // Todo: cambiar el número de 100 a 10 o cualquier otro número
   // pero el porcentaje del radial-progress debe de ser correcto basado en ese numero
   const [seconds, setSeconds] = useState(initialValue);
+  const [isRunning, setIsRunning] = useState(false)
+  // const percentage = seconds; 
+  // Todo: Realizar el calculo
+  const percentage = 100 - (seconds * 100 / initialValue);
+  const timerRef = useRef<number>();
+
+  const startTimer = () => {
+    // Todo:
+    // 1. Si el timer esta corriendo, no hacer nada
+    if (isRunning) return;
+
+    // 2. Si el timer no esta corriendo, iniciar el timer
+    setIsRunning(true);
+
+    // 3. Si el timer no esta corriendo, iniciar el timer
+    const timer = window.setInterval(() => {
+      setSeconds((prevSeconds) => {
+        if (prevSeconds === 0) {
+          window.clearInterval(timer);
+          setIsRunning(false);
+          return prevSeconds;
+        }
+        return prevSeconds - 1;
+      })
+    }, 1000);
+  }
+
+  const pauseTimer = () => {
+    // Todo:
+    // 1. Si el timer no esta corriendo, no hacer nada
+    if (!isRunning) return;
+
+    // 2. Si el timer esta corriendo, detener el timer
+    setIsRunning(false);
+  }
+
+  const restartTimer = () => {
+    // Todo:
+
+    // 1. Si el timer esta corriendo, detener el timer
+    window.clearInterval(timerRef.current);
+    // 2. Si el timer esta corriendo, detener el timer
+    setIsRunning(false);
+    // 3. Si el timer esta corriendo, detener el timer
+    setSeconds(initialValue);
+  }
 
 
-  const percentage = seconds; // Todo: Realizar el calculo
-
-  
   return (
     <div className="w-4/12 flex flex-col items-center">
-        
-        {/* Radial Primario */}
-        <div 
-          className="radial-progress bg-secondary text-primary-content border-4 border-secondary text-3xl text-center" 
-          style={{'--value': percentage, "--size":'12rem', '--thickness':'2rem'}}>
-            <span>{ seconds }</span>
-            <span className='text-sm'>secs</span>
-        </div>
 
-        {/* Radial para hacer efecto de sombra */}
-        <div 
-          className="absolute radial-progress text-primary-content opacity-40 border-4 border-secondary" 
-          style={{'--value':100, "--size":'12rem', '--thickness':'2rem'}}></div>
+      {/* Radial Primario */}
+      <div
+        className="radial-progress bg-secondary text-primary-content border-4 border-secondary text-3xl text-center"
+        style={{ '--value': percentage, "--size": '12rem', '--thickness': '2rem' }}>
+        <span>{seconds}</span>
+        <span className='text-sm'>
+          {/* Cuando falten 10 segundos, pasar a porcentaje */}
+          {seconds <= 10 ? `${percentage.toFixed(0)}%` : 'Segundos'}
+        </span>
+      </div>
 
-        <div className="divider"></div>
+      {/* Radial para hacer efecto de sombra */}
+      <div
+        className="absolute radial-progress text-primary-content opacity-40 border-4 border-secondary"
+        style={{ '--value': 100, "--size": '12rem', '--thickness': '2rem' }}></div>
 
-        <div className="flex mt-2 gap-2">
-          
-        <button 
-            className="btn btn-accent">
-            <RiPlayLine />
-          </button>
+      <div className="divider"></div>
 
-          <button className="btn">
-            <RiPauseFill />
-          </button>
-          
-          <button className="btn hover:btn-accent">
-            <RiRestartLine />
-          </button>
+      <div className="flex mt-2 gap-2">
 
-        </div>
+        <button
+          onClick={() => startTimer()}
+          className="btn btn-accent">
+          <RiPlayLine />
+        </button>
+
+        <button
+          onClick={() => pauseTimer()}
+          className="btn ">
+          <RiPauseFill />
+        </button>
+
+        <button
+          onClick={() => restartTimer()}
+          className="btn ">
+          <RiRestartLine />
+        </button>
+
+      </div>
 
 
     </div>
